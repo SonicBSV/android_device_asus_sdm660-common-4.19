182dece73fc3cb9abba6603409ef1da062e0f400
SonicBSV <sonicbsv@gmail.com>
2023-01-26 22:38:57 +0300

    sdm660-common: update health rules

--- sdm660.mk
+++ sdm660.mk
@@ -182,8 +182,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery
 
 # GMS Permissions
 PRODUCT_COPY_FILES += \
