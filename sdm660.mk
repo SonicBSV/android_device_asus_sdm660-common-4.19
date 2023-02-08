b99170769b6e518e6cc4ecdde69961c8106b20ef
SonicBSV <sonicbsv@gmail.com>
2023-02-08 21:54:32 +0300

    sdm660-common: update Telephony rules

--- sdm660.mk
+++ sdm660.mk
@@ -471,15 +471,18 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
+    qtiImsInCallUi \
+    ConfURIDialer \
     telephony-ext
 
 # Thermal
