94767223a8ff8e26b5c447f28102992d0b4da7b8
SonicBSV <sonicbsv@gmail.com>
2023-07-02 01:10:43 +0300

    sdm660-common: Makefile: Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.
    taken from: https://github.com/AlphaDroid-devices/device_xiaomi_mido/commit/715cdf88ad5b16ea69acd509d62d617f47a1f1d4

--- sdm660.mk
+++ sdm660.mk
@@ -232,6 +232,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Input
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
