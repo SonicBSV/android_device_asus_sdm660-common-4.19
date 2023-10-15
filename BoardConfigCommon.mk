e07f2eebd0c59928dd81b52f40c7f0ba03a43e70
SonicBSV <sonicbsv@gmail.com>
2023-10-15 06:35:23 +0300

    sdm660-common: BoardConfigCommon: KernelSU enabled by default

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,7 +139,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
-BOARD_KERNEL_CMDLINE += kernelsu.enabled=0
+BOARD_KERNEL_CMDLINE += kernelsu.enabled=1
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_BASE := 0x00000000
